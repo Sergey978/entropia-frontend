9fa4b5a841e668cb62f125e583eb509244917e2f
Sergey <vasilevsky.s@gmail.com>
2021-11-07 13:36:38 +0200

    PC header working good

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
 
-interface IProps {}
+interface IProps { }
 
 interface IState {
   isAuthorized?: boolean;
@@ -19,75 +19,115 @@ export default class Header extends Component<IProps, IState> {
   render() {
     return (
       <header className="header-global">
-      <nav id="navbar-main" aria-label="Primary navigation" className="navbar navbar-main navbar-expand-lg navbar-theme-primary headroom navbar-light navbar-transparent navbar-theme-primary">
+        <nav
+          id="navbar-main"
+          aria-label="Primary navigation"
+          className="navbar navbar-main navbar-expand-lg navbar-theme-primary headroom navbar-light navbar-transparent navbar-theme-primary"
+        >
           <div className="container position-relative">
-              <a className="navbar-brand shadow-soft py-2 px-3 rounded border border-light mr-lg-4" href="./index.html">
-                  <img className="navbar-brand-dark" src="./assets/img/brand/dark.svg" alt="Logo light"/>
-                  <img className="navbar-brand-light" src="./assets/img/brand/dark.svg" alt="Logo dark"/>
-              </a>
-              <div className="navbar-collapse collapse" id="navbar_global">
-                  <div className="navbar-collapse-header">
-                      <div className="row">
-                          <div className="col-6 collapse-brand">
-                              <a href="./index.html" className="navbar-brand shadow-soft py-2 px-3 rounded border border-light">
-                                  <img src="./assets/img/brand/dark.svg" alt="Themesberg logo"/>
-                              </a>
-                          </div>
-                          <div className="col-6 collapse-close">
-                              <a href="#navbar_global" className="fas fa-times" data-toggle="collapse" data-target="#navbar_global" aria-controls="navbar_global" aria-expanded="false" title="close" aria-label="Toggle navigation"></a>
-                          </div>
-                      </div>
+            <Link
+              className="navbar-brand shadow-soft py-2 px-3 rounded border border-light mr-lg-4"
+              to="./index.html"
+            >
+              <img
+                className="navbar-brand-dark"
+                src="./assets/img/brand/dark.svg"
+                alt="Logo light"
+              />
+              <img
+                className="navbar-brand-light"
+                src="./assets/img/brand/dark.svg"
+                alt="Logo dark"
+              />
+            </Link>
+            <div className="navbar-collapse collapse" id="navbar_global">
+              <div className="navbar-collapse-header">
+                <div className="row">
+                  <div className="col-6 collapse-brand">
+                    <Link
+                      to="./index.html"
+                      className="navbar-brand shadow-soft py-2 px-3 rounded border border-light"
+                    >
+                      <img
+                        src="./assets/img/brand/dark.svg"
+                        alt="Themesberg logo"
+                      />
+                    </Link>
                   </div>
-                  <ul className="navbar-nav navbar-nav-hover align-items-lg-center">
-                      <li className="nav-item ">
-                          <Link to="/" className="nav-link" >
-                              <span className="nav-link-inner-text">Home</span>                              
-                          </Link>
-                      </li>
-                      <li className="nav-item">
-                          <Link to="/about" className="nav-link"  >
-                              <span className="nav-link-inner-text">About</span>                              
-                          </Link>
-                          
-                      </li>
-                      <li className="nav-item dropdown">
-                          <a href="#" className="nav-link" data-toggle="dropdown" >
-                              <span className="nav-link-inner-text">Support</span>
-                              <span className="fas fa-angle-down nav-link-arrow ml-2"></span>
-                          </a>
-                          <div className="dropdown-menu dropdown-menu-lg">
-                              <div className="col-auto px-0" data-dropdown-content>
-                                  <div className="list-group list-group-flush">
-                                      <a href="https://themesberg.com/docs/neumorphism-ui/getting-started/quick-start/" target="_blank" className="list-group-item list-group-item-action d-flex align-items-center p-0 py-3 px-lg-4">
-                                          <span className="icon icon-sm icon-secondary"><span className="fas fa-file-alt"></span></span>
-                                          <div className="ml-4">
-                                              <span className="text-dark d-block">Documentation<span className="badge badge-sm badge-secondary ml-2">v1.0</span></span>
-                                              <span className="small">Examples and guides</span>
-                                          </div>
-                                      </a>
-                                      <a href="https://github.com/themesberg/th-neumorphism-ui-pro/issues" target="_blank" className="list-group-item list-group-item-action d-flex align-items-center p-0 py-3 px-lg-4">
-                                          <span className="icon icon-sm icon-secondary"><span className="fas fa-microphone-alt"></span></span>
-                                          <div className="ml-4">
-                                              <span className="text-dark d-block">Support</span>
-                                              <span className="small">Looking for answers? Ask us!</span>
-                                          </div>
-                                      </a>
-                                  </div>
-                              </div>
-                          </div>
-                      </li>
+                  <div className="col-6 collapse-close">
+                    <Link
+                      to="#navbar_global"
+                      className="fas fa-times"
+                      data-toggle="collapse"
+                      data-target="#navbar_global"
+                      aria-controls="navbar_global"
+                      aria-expanded="false"
+                      title="close"
+                      aria-label="Toggle navigation"
+                    ></Link>
+                  </div>
+                </div>
+              </div>
+              <ul className="navbar-nav navbar-nav-hover align-items-lg-center">
+                <li className="nav-item ">
+                  <Link to="/" className="nav-link">
+                    <span className="nav-link-inner-text">Home</span>
+                  </Link>
+                </li>
+                <li className="nav-item">
+                  <Link to="/about" className="nav-link">
+                    <span className="nav-link-inner-text">About</span>
+                  </Link>
+                </li>
+                <li className="nav-item dropdown">
+                  <Link to="/" className="nav-link" data-toggle="dropdown">
+                    <span className="nav-link-inner-text">Items</span>
+                    <span className="fas fa-angle-down nav-link-arrow ml-2"></span>
+                  </Link>
+                  <ul className="dropdown-menu">
+                    <li>
+                      <Link className="dropdown-item" to="/select-items">Select Items</Link>
+                    </li>
+                    <li>
+                      <Link className="dropdown-item"  to="/custom-items">Custom Items </Link>
+                    </li>
+                    <li>
+                      <Link className="dropdown-item"  to="/graph-page">Graph</Link>
+                    </li>                      
                   </ul>
-              </div>
-              <div className="d-flex align-items-center">
-                  <a href="https://themesberg.com/product/ui-kits/neumorphism-ui-pro" target="_blank" className="btn btn-primary text-secondary mr-3"><i className="far fa-paper-plane mr-2"></i> Upgrade to PRO</a>
-                  <a href="https://themesberg.com/docs/neumorphism-ui/getting-started/quick-start/" target="_blank" className="btn btn-primary d-none d-md-inline-block"><i className="fas fa-book"></i> Docs v1.0</a>
-                  <button className="navbar-toggler ml-2" type="button" data-toggle="collapse" data-target="#navbar_global" aria-controls="navbar_global" aria-expanded="false" aria-label="Toggle navigation">
-                      <span className="navbar-toggler-icon"></span>
-                  </button>
-              </div>
+                </li>
+              </ul>
+            </div>
+            <div className="d-flex align-items-center">
+              <Link
+                to="https://themesberg.com/product/ui-kits/neumorphism-ui-pro"
+                target="_blank"
+                className="btn btn-primary text-secondary mr-3"
+              >
+                <i className="far fa-paper-plane mr-2"></i> Upgrade to PRO
+              </Link>
+              <Link
+                to="https://themesberg.com/docs/neumorphism-ui/getting-started/quick-start/"
+                target="_blank"
+                className="btn btn-primary d-none d-md-inline-block"
+              >
+                <i className="fas fa-book"></i> Docs v1.0
+              </Link>
+              <button
+                className="navbar-toggler ml-2"
+                type="button"
+                data-toggle="collapse"
+                data-target="#navbar_global"
+                aria-controls="navbar_global"
+                aria-expanded="false"
+                aria-label="Toggle navigation"
+              >
+                <span className="navbar-toggler-icon"></span>
+              </button>
+            </div>
           </div>
-      </nav>
-  </header>
+        </nav>
+      </header>
     );
   }
 }
--- src/components/pages/graph-page.tsx
+++ src/components/pages/graph-page.tsx
@@ -9,7 +9,7 @@ const GraphPage = () => {
         <div className="container">
           <div className="row justify-content-center">
             <div className="col-12 col-lg-8 text-center">
-              <h1 className="display-2 mb-4">Custom Item Page</h1>
+              <h1 className="display-2 mb-4">Graph Page</h1>
               <p className="lead mb-5"></p>
               <a className="btn btn-primary" href="./contact.html">
                 <span className="fas fa-book-reader mr-2"></span>Our works
