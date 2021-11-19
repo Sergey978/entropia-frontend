8f188410f3655a5442d1a2167823667bb6bc82fd
Sergey <vasilevsky.s@gmail.com>
2021-11-19 20:59:17 +0300

    custom item page in progress

--- src/components/item-list/item-list.tsx
+++ src/components/item-list/item-list.tsx
@@ -7,23 +7,34 @@ interface Props {
 
 }
 
-  
+
 export const ItemList = ({ data }: Props) => (
 
-  <ul>
-  
+
+  <table className="table table-hover shadow-inset rounded">
+    <tbody>
+      <tr>
+        <th className="border-0" scope="col" >Name</th>
+        <th className="border-0" scope="col" >Cost</th>
+        <th className="border-0" scope="col" ></th>
+      </tr>
+
       {
         data.map((item) => (
-          <li key={item.itemId}   >
-            {item.itemName}:"   "{item.price}
-          </li>
-
+          <tr key={item.itemId}>
+            <td key={item.itemId + "_1"}>{item.itemName}</td>
+            <td key={item.itemId + "_2"} >{item.price}</td>
+            <td key={item.itemId + "_3"}>
+            <span className="btn far fa-lg fa-trash-alt  btn-outline-danger float-right"></span>
+              <span className="btn far fa-lg fa-edit btn-outline-success float-right" ></span>
+              <span className="btn far fa-lg fa-eye-slash btn-outline-secondary float-right"></span>
+              
+            </td>
+          </tr>
         ))
-     
-
-    }
-
-  </ul>
+      }
+    </tbody>
+  </table>
 
 );
 
--- src/components/pages/custom-items-page.tsx
+++ src/components/pages/custom-items-page.tsx
@@ -39,87 +39,39 @@ const CustomItemsPage = () => {
                     <div className="row mb-4 justify-content-center">
                         <div className="col-12 col-md-8 col-lg-7">
                             <h1 className="display-3 mb-4">Custom Items</h1>
-                            <p className="lead">Pixel makes use of the <strong>"Nunito Sans"</strong> Google Web Font for beatiful and readable typography.</p>
-                        </div>
-                    </div>
-                    <div className="row justify-content-center">
-                        <div className="col-12 col-md-10 col-lg-6">
-                            <div className="alert shadow-inset">
-                                <div className="d-flex">
-                                    <div className="alert-inner--text">
-                                        <span className="icon text-danger icon-xs mr-2 mr-md-1"><span className="fas fa-file-alt"></span></span>
-                                        <a target="_blank" href="https://themesberg.com/docs/neumorphism-ui/foundation/typography/">See our Documentation</a> for a detailed technical explanation.
+
+                            <div className="row justify-content-center">
+                                <div className="col-lg-12">
+                                    <div className="mb-6">
+                                        <ItemList data={customItems} />
+                                        {/*v<!-- Form -->*/}
+                                        <form>
+
+                                            <div className="row mb-4">
+                                                <div className="col">
+                                                    <div className="form-outline">
+                                                        <input type="text" id="form3Example1" className="form-control" />
+                                                        <label className="form-label" htmlFor="form3Example1">First name</label>
+                                                    </div>
+                                                </div>
+                                                <div className="col">
+                                                    <div className="form-outline">
+                                                        <input type="text" id="form3Example2" className="form-control" />
+                                                        <label className="form-label" htmlFor="form3Example2">Last name</label>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </form>
+                                        {/*<!-- End of Form -->*/}
                                     </div>
                                 </div>
                             </div>
                         </div>
                     </div>
+
                 </div>
             </div>
             {/* <!-- End of Hero -->   */}
-
- {/* <!-- ToDo make table from this list -->   */}
-
- <ItemList data={customItems} /> 
-
-
-
-
-            <div className="section bg-primary text-dark section-lg">
-                <div className="container">
-                    <div className="row justify-content-center">
-                        <div className="col-lg-10">
-                            <div className="mb-5">
-
-
-                                <table className="table table-hover shadow-inset rounded">
-                                    <tbody>
-                                        <tr>
-                                            <th className="border-0" scope="col" id="class2">Name</th>
-                                            <th className="border-0" scope="col" id="teacher2">Cost</th>
-                                            <th className="border-0" scope="col" id="males2"></th>
-
-                                        </tr>
-                                        <tr>
-                                            <td scope="row" id="firstyear2">First Year</td>
-                                            <td scope="row" id="Bolter2" headers="firstyear2">D. Bolter</td>
-                                            <td headers="firstyear2 Bolter2 males2">5</td>
-
-                                        </tr>
-                                        <tr>
-
-                                            <td headers="firstyear2 Cheetham2 males2">7</td>
-                                            <td headers="firstyear2 Cheetham2 females2">9</td>
-                                            <td headers="firstyear2 Cheetham2 females2">9</td>
-                                        </tr>
-                                        <tr>
-
-                                            <td headers="firstyear2 Cheetham2 males2">7</td>
-                                            <td headers="firstyear2 Cheetham2 females2">9</td>
-                                            <td headers="firstyear2 Cheetham2 females2">9</td>
-                                        </tr>
-                                        <tr>
-
-                                            <td headers="firstyear2 Cheetham2 males2">7</td>
-                                            <td headers="firstyear2 Cheetham2 females2">9</td>
-                                            <td headers="firstyear2 Cheetham2 females2">9</td>
-                                        </tr>
-                                        <tr>
-
-                                            <td headers="firstyear2 Cheetham2 males2">7</td>
-                                            <td headers="firstyear2 Cheetham2 females2">9</td>
-                                            <td headers="firstyear2 Cheetham2 females2">9</td>
-                                        </tr>
-                                    </tbody>
-                                </table>
-
-
-
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
 
 
         </Page>
