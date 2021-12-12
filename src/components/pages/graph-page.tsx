93d32b3f71db8ad0c7a6054093800e905379ccac
Sergey978 <vasilevsky.s@gmail.com>
2021-12-12 13:52:51 +0200

    graph form in progress

--- src/components/pages/graph-page.tsx
+++ src/components/pages/graph-page.tsx
@@ -10,15 +10,88 @@ const GraphPage = () => {
           <div className="row justify-content-center">
             <div className="col-12 col-lg-8 text-center">
               <h1 className="display-2 mb-4">Graph Page</h1>
-              <p className="lead mb-5"></p>
-              <a className="btn btn-primary" href="./contact.html">
-                <span className="fas fa-book-reader mr-2"></span>Our works
-              </a>
+
             </div>
           </div>
         </div>
       </div>
       {/*<!-- End of Hero section -->*/}
+
+
+      <section className="section section-lg pt-0">
+        <div className="container">
+          <div className="row">
+            <div className="col">
+              <div className="card bg-primary shadow-soft border-light p-4">
+
+                <div className="col-12 col-lg-4 px-md-0 mb-4 mb-lg-0">
+                  <div className="card-body  bg-primary py-5">
+
+
+                    <div className="form-row">
+                      <div className="form-group col-6 ">
+                        <label className="my-1 mr-2" htmlFor="itemCost">Item cost</label>
+                        <input className="form-control" id="itemCost" />
+                      </div>
+                      <div className="form-group col">
+                        <label className="my-1 mr-2" htmlFor="price">Price</label>
+                        <input className="form-control" id="price" />
+                      </div>
+                    </div>
+
+
+
+
+                  </div>
+                </div>
+
+
+
+
+                <div className="col-12 col-lg-4 px-md-0 mb-4 mb-lg-0">
+                  <div className="card-body  bg-primary py-5">
+
+
+                    <div className="form-row">
+                      <div className="form-group col-6 ">
+                        <label className="my-1 mr-2" htmlFor="itemCost">Item cost</label>
+                        <input className="form-control" id="itemCost" />
+                      </div>
+                      <div className="form-group col">
+                        <label className="my-1 mr-2" htmlFor="price">Price</label>
+                        <input className="form-control" id="price" />
+                      </div>
+                    </div>
+
+
+
+
+                  </div>
+                </div>
+
+
+
+                <div className="col-12 col-lg-4 px-md-0 mb-4 mb-lg-0">
+                  <div className="card-body  bg-primary py-5">
+                  </div>
+                </div>
+
+
+
+
+
+
+
+
+
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+
+
+
     </Page>
   );
 };
