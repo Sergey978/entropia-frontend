43ed669ce3bfdbd73d06b111ca673f6720bf3644
Sergey978 <vasilevsky.s@gmail.com>
2022-02-16 18:08:05 +0200

    graph in progress, scroll started

--- src/components/graph-container/graph-container.tsx
+++ src/components/graph-container/graph-container.tsx
@@ -1,4 +1,4 @@
-import React, { Component, useState, useEffect, useCallback, WheelEvent } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import ReactDOM from 'react-dom';
 
 import "./graph-container.css";
@@ -34,55 +34,108 @@ const GraphContainer = ({ table }: Props) => {
 
     const [chartState, setChartState] = useState(chartParams);
 
-    const scaleCallback = useCallback((e:  WheelEvent) => {       
-        
-        
-        e.preventDefault();           
-        
-        console.log(            
-            e,
-            e.deltaY,
-            
-        );
+    const zoomRef = useRef<HTMLDivElement>(null);
 
-    }, [])
+    const scroll = (e: WheelEvent) => {
+        console.log(e.deltaY);
+        const scrollX = zoomRef.current?.scrollTop;
+        const scrollY = zoomRef.current?.scrollTop;
+        const kx0 = chartState.kx;
+        const ky0 = chartState.ky;
+
+        if (e.deltaY > 0) {
+
+            setChartState((chartState) => ({
+                ...chartState,
+                kx: chartState.kx + (chartState.kx / 100) * 5,
+                ky: chartState.ky + (chartState.ky / 100) * 5,
+
+            }));
+        }
+        else if (chartState.kx > 5 && chartState.ky > 5) {
+            setChartState((chartState) => ({
+                ...chartState,
+                kx: chartState.kx - (chartState.kx / 100) * 5,
+                ky: chartState.ky - (chartState.ky / 100) * 5
+
+            }));
+        }
+
+        // отмасштабируем при помощи CSS
+        //     if (delta > 0) {
+        //         kx0 = item.kx;
+        //         ky0 = item.ky;
+        //         item.kx = item.kx + (item.kx / 100) * 5;
+        //         item.ky = item.ky + (item.ky / 100) * 5;
+        //         $(graphContainer)
+        //             .scrollLeft(100 + params.oxn + (scrollX - 100 - params.oxn) * item.kx / kx0);
+
+        //         $(graphContainer)
+        //             .scrollTop(params.oyn + params.ly + (scrollY - params.oyn - params.ly) * item.ky / ky0);
+        //     }
+        //     else
+        //         if (item.kx > 5 && item.ky > 5) {
+        //             kx0 = item.kx;
+        //             ky0 = item.ky;
+        //             item.kx = item.kx - (item.kx / 100) * 5;
+        //             item.ky = item.ky - (item.ky / 100) * 5;
+
+        //             $(graphContainer)
+        //              .scrollLeft(100 + params.oxn + (scrollX - 100 - params.oxn) * item.kx / kx0);
+
+        //             $(graphContainer)
+        //                 .scrollTop(params.oyn + params.ly + (scrollY - params.oyn - params.ly) * item.ky / ky0);
+
+        //         };
+        //     redrawChart();
+        //     // отменим прокрутку 
+        //     e.preventDefault();
+        // });
+
+
+
+        e.preventDefault();
+    };
 
 
     useEffect(() => {
+        const currentZoomRef = zoomRef?.current;
 
-        const graphArea = document.getElementById('graph-container');
-        graphArea!.addEventListener('wheel', scaleCallback);
-        return () => graphArea!.removeEventListener('wheel', scaleCallback);
-    }, [scaleCallback]);
+        currentZoomRef?.addEventListener("wheel", scroll);
+
+        return () => {
+            currentZoomRef?.removeEventListener("wheel", scroll);
+        };
+    }, []);
 
 
     // <!-- GraphConainer -->
     return (
-        <div className="graph-container" id="graph-container"  >
+        <div className="graph-container" ref={zoomRef}  >
             <div className="graph" id="graph">
 
 
                 <div className="horizontal-axis"
                     style={{
-                        width: chartParams.lx,
-                        left: chartParams.oxn,
-                        top: chartParams.oyn + chartParams.ly
+                        width: chartState.lx,
+                        left: chartState.oxn,
+                        top: chartState.oyn + chartParams.ly
                     }}
                 ></div>
 
                 <div className="vertical-axis"
                     style={{
-                        height: chartParams.ly,
-                        left: chartParams.oxn,
-                        top: chartParams.oyn
+                        height: chartState.ly,
+                        left: chartState.oxn,
+                        top: chartState.oyn
                     }}
                 ></div>
 
-                <Grid  {...chartParams} />
+                <Grid  {...chartState} />
 
                 {table.map((row) => (
 
-                    <Point tableRow={row} chartParams={chartParams} key={"point-" + row.Quantity} />
+                    <Point tableRow={row} chartParams={chartState} key={"point-" + row.Quantity} />
 
                 ))
                 }
